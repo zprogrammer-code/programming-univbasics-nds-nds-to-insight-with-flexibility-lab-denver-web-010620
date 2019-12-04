2127002b1137773b4aec87d1d14ad07c92dc92ea
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-04 20:18:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -59,12 +59,10 @@ def gross_per_studio(collection)
    gross = collection[row_index][:worldwide_gross]
    total_gross += gross
  
-   result[studio] = total_gross
  
-   
    row_index += 1 
  end
- 
+ result[studio] = total_gross
  return result
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
